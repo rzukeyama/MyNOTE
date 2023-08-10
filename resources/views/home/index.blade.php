594fe7d38bed0e69b8d0af0246bc51587d67d4fa
r-zukeyama <r-zukeyama@atelieraz-okinawa.com>
2023-08-10 14:33:27 +0900

    - デザイン変更

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,84 +4,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ホーム | {{config('app.name')}}</title>
-    <link rel="stylesheet" href="./bootstrap.css">
+    <link rel="stylesheet" href="./sample.css">
 </head>
 <body>
 
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">{{config('app.name')}}</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        機能
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        @foreach ($userFunctions as $func)
+                            <li><a class="dropdown-item" href="/{{$func->function->route}}">{{$func->function->name}}</a></li>
+                        @endforeach
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a href="/user_functions" class="dropdown-item">機能設定</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ Auth::user()->display_name ?? 'ユーザー' }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a href="/user/change_password" class="dropdown-item">パスワードの変更</a></li>
+                        <li><a href="/user/change_name" class="dropdown-item">表示名の設定/変更</a></li>
+                        <li><a href="/user/delete" class="dropdown-item text-danger">{{config('app.name')}}をやめる(利用者情報の完全削除)</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a href="/user/logout" class="dropdown-item">利用を終了する(ログアウト)</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <div class="container">
 
-    @if (!is_null(Auth::user()->display_name))
-        <div class="row">
-            <div class="col">
-                ようこそ {{ Auth::user()->display_name }} さん！
-            </div>
-        </div>
-    @endif
-
+@if (!is_null(Auth::user()->display_name))
     <div class="row mt-2">
         <div class="col">
-            <div class="h1">{{config('app.name')}}</div>
-            <p>
-                {{config('app.name')}}は、あなただけのプライベートノート。あなたに関する記録はなんでもおまかせください！
-            </p>
+            ようこそ {{ Auth::user()->display_name }} さん！
         </div>
     </div>
-
-    <div class="row mt-2">
-        <div class="col">
-            @if (session('error'))
-            <div class="alert alert-warning">
-                {{ session('error') }}
-            </div>
-            @endif
-            @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-            @endif
-            @if ($errors->any())
-            <div class="alert alert-warning">
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </div>
-            @endif
-        </div>
-    </div>
-
-    <div class="row mt-2">
-        <div class="col">
-            @foreach ($userFunctions as $func)
-            <a href="/{{$func->function->route}}" class="btn btn-info">{{$func->function->name}}</a>
-            @endforeach
-        </div>
-    </div>
-
-    <div class="row mt-2">
-        <div class="col">
-            <a href="/user/logout" class="btn btn-dark">利用を終了する(ログアウト)</a>
-        </div>
-    </div>
-
-    <div class="row mt-5">
-        <div class="col">
-            <h2>利用者設定</h2>
-            <p>
-                パスワードの変更やアプリの追加はこちらからどうぞ
-            </p>
-        </div>
-    </div>
-
-    <div class="row mt-2">
-        <div class="col">
-            <a href="/user/change_password" class="btn btn-secondary">パスワードの変更</a>
-            <a href="/user/change_name" class="btn btn-secondary">表示名の設定/変更</a>
-            <a href="/user_functions" class="btn btn-secondary">機能設定</a>
-            <a href="/user/delete" class="btn btn-outline-danger">{{config('app.name')}}をやめる(利用者情報の完全削除)</a>
-        </div>
-    </div>
-
+@endif
 </div>
 
+<script src="./js/bootstrap.min.js"></script>
 </body>
 </html>
--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>利用者認証 | {{config('app.name')}}</title>
-    <link rel="stylesheet" href="./bootstrap.css">
+    <link rel="stylesheet" href="./sample.css">
 </head>
 <body>
 
